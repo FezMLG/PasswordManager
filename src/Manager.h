e1fd13e2297c6a691d3a586674a4fcf84abe7ba6
FezMLG <konradchw@gmail.com>
2022-12-19 23:18:10 +0100

    comments for Manager.h

--- src/Manager.h
+++ src/Manager.h
@@ -22,24 +22,55 @@ private:
 public:
     Manager(Categories *categories1);
 
+    /**
+     * @brief Add new password entry to entries
+     * @param entry
+     */
     void pushToEntries(const Entry &entry);
 
     vector<Entry> getEntries();
 
     void setEntries(vector<Entry> &entries);
 
+    /**
+     * @brief Showing form to create new Entry
+     */
     void createNewEntryForm();
 
+    /**
+     * @brief Showing form to remove Entry from vector
+     */
     void removeEntryForm();
 
+    /**
+     * @brief Form to select and edit entry with execution
+     */
     void editEntryForm();
 
+    /**
+     * @brief Entries filtering
+     * @param option
+     * @param filteredEntries
+     */
     void searchForEntryForm(int option, vector<Entry> *filteredEntries);
 
+    /**
+     * @brief Form to filter entries
+     * @param newEntries
+     */
     void searchForm(vector<Entry> newEntries);
 
+    /**
+     * @brief Returning Entry with given name
+     * @param name of the Entry
+     * @return Entry class
+     */
     Entry getEntryWithName(const string &name);
 
+    /**
+     * @brief removing entry from vector
+     * @param name
+     */
     void removeEntry(const string &name);
 
     void setPassword(string &newPassword);
@@ -54,17 +85,42 @@ public:
 
     Categories *getCategories();
 
+    /**
+     * @brief Getting all Entries name
+     * @return names
+     */
     vector<string> getNames();
 
+    /**
+     * @brief Printing all Entries names to console
+     */
     void printEntriesName();
 
+    /**
+     * @brief Form for deleting category with deleting category and all Entries with it
+     */
     void deleteCategoryForm();
 
+    /**
+     * @brief Form for handling new password
+     * @return password
+     */
     static string passwordForm();
 };
 
+/**
+ * @brief Form for getting new value for Entry or keeping existing and returning as new
+ * @param valueName name of Entry attribute
+ * @param oldValue old value of Entry attribute
+ * @param newValue variable that holds new attribute
+ * @return string with new attribute
+ */
 string getEditNewValueForm(const string &valueName, const string &oldValue, string newValue);
 
+/**
+ * @brief Form for creating new category
+ * @param categories Categories class
+ */
 void newCategoryForm(Categories *categories);
 
 #endif //PASSWORDMANAGER_MANAGER_H
