5175e9c3dd6c567280bbeff750c4dc5110a54fcd
FezMLG <konradchw@gmail.com>
2022-12-22 19:27:59 +0100

    comments for App

--- src/App.h
+++ src/App.h
@@ -8,14 +8,33 @@
 #include <iostream>
 #include "Manager.h"
 
+/**
+ * @brief starting point for program
+ */
 void start();
 
+/**
+ * @brief manual encryption/decryption of file, to enable, change manualEncryptMode flag
+ * @param manager
+ */
 void manualEncryption(Manager *manager);
 
+/**
+ * @brief getting file path and password from user
+ * @param manager Manager class initialized in start point
+ */
 void getFileAndPassword(Manager *manager);
 
+/**
+ * @brief printing main manu to console
+ * @param manager Manager class initialized in start point
+ */
 void mainMenu(Manager *manager);
 
+/**
+ * @brief handling option chosen from main menu by user
+ * @param manager Manager class initialized in start point
+ */
 void handleOption(Manager *manager);
 
 #endif //PASSWORDMANAGER_APP_H
