6d7603385d030b35a781712cbc74749c97cb2def
FezMLG <konradchw@gmail.com>
2022-12-22 19:19:55 +0100

    comments for entry

--- src/Entry.h
+++ src/Entry.h
@@ -24,8 +24,15 @@ public:
     Entry(const std::string &name, const std::string &login, const std::string &password, const std::string &category,
           const std::string &type, const std::string &service);
 
+    /**
+     * @brief Print entry to console
+     */
     void print();
 
+    /**
+     * @brief Generate entry with formatting for saving in file
+     * @return formatted string for file
+     */
     string printForFile();
 
     string getName();
@@ -40,8 +47,18 @@ public:
 
     string getService();
 
+    /**
+     * @brief generating random password
+     * @param length length of the generated password
+     * @return generated password
+     */
     static string generatePassword(int &length);
 
+    /**
+     * @brief checking if password contains lowercase, uppercase, number, special char, length more than 8 and more than 16, and returning password strength on scale 0-6
+     * @param passwordToCheck
+     * @return password strength between 0-6, where 0 is weak and 6 is strong
+     */
     static int checkPasswordStrength(const std::string &passwordToCheck);
 };
 
